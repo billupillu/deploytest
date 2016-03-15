4028ac626ce60cc1cccf4b2aa3697653e9f3be8a
Sumeet Masih <billu.pillu123@gmail.com>
2016-03-15 16:48:33 +0530

    deploy test 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.4.0'
 
-set :application, 'contactbook'
-set :repo_url, 'https://github.com/billupillu/onetwokafour' # Edit this to match your repository
+set :application, 'anzels'
+set :repo_url, 'git@github.com:billupillu/deploytest.git' # Edit this to match your repository
 set :branch, :master
 set :deploy_to, '/home/deploy/contactbook'
 set :pty, true
@@ -9,7 +9,7 @@ set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
-set :rvm_ruby_version, 'jruby-1.7.19' # Edit this if you are using MRI Ruby
+set :rvm_ruby_version, 'ruby-2.2.1' # Edit this if you are using MRI Ruby
 
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
@@ -25,3 +25,4 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
+
