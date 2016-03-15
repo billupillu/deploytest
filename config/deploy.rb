753d75a2714a6bc456494151b65baab8c16fcc6b
Sumeet Masih <billu.pillu123@gmail.com>
2016-03-15 18:06:38 +0530

    deploy test 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.4.0'
 set :application, 'anzels'
 set :repo_url, 'git@github.com:billupillu/deploytest.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/contactbook'
+set :deploy_to, '/home/deploy/anzels'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
